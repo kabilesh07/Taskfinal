a38538dce8d0957095241c6bc297b0660bf481a4
kabilesh07 <98596684+kabilesh07@users.noreply.github.com>
2022-03-30 16:24:49 +0530

    Add files via upload
    Changing the position

---
+++ Taskfinal-Changingtheposition/src/main/java/com/example/bio/Mainadapter.java
@@ -0,0 +1,73 @@
+package com.example.bio;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+public class Mainadapter extends RecyclerView.Adapter<Mainadapter.ViewHolder> {
+    ArrayList<Mainmodel> mainmodels;
+    MainActivity context;
+    RecyclerView recyclerView;
+
+    public Mainadapter(ArrayList<Mainmodel> mainmodels, MainActivity context, RecyclerView recyclerView) {
+        this.mainmodels = mainmodels;
+        this.context = context;
+        this.recyclerView = recyclerView;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from( parent.getContext() ).inflate( R.layout.row_item, parent, false );
+        return new ViewHolder( view );
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.imageView.setImageResource( mainmodels.get( position ).getR() );
+        holder.textView.setText( mainmodels.get( position ).getLangname() );
+
+        holder.itemView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayoutManager layoutManager  = (LinearLayoutManager) recyclerView.getLayoutManager();
+                if (layoutManager != null) {
+                    layoutManager.scrollToPositionWithOffset( position, 0 );
+                }
+            }
+        } );
+
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+
+        return mainmodels.size();
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public Object setOnclick;
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super( itemView );
+            //getting image and text view
+            imageView = itemView.findViewById( R.id.image_view );
+            textView = itemView.findViewById( R.id.Textview );
+        }
+    }
+}
