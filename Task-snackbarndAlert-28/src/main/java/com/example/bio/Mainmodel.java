5cdea75cf426258a87fb1c11c2bc4ffc2e5e4894
kabilesh07 <98596684+kabilesh07@users.noreply.github.com>
2022-03-30 16:23:23 +0530

    Add files via upload
    Snack and alert

---
+++ Task-snackbarndAlert-28/src/main/java/com/example/bio/Mainmodel.java
@@ -0,0 +1,24 @@
+package com.example.bio;
+
+public class Mainmodel {
+
+    Integer r;
+    String langname;
+
+
+
+    public Mainmodel(Integer r, String langname) {
+        this.r = r;
+        this.langname = langname;
+    }
+
+    public Integer getR() {
+
+        return r;
+    }
+
+    public String getLangname() {
+
+        return langname;
+    }
+}
