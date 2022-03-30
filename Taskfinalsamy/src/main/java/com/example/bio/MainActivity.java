425ce428a043ec3f97643fecd5774256dc25a2c7
kabilesh07 <98596684+kabilesh07@users.noreply.github.com>
2022-03-30 16:27:10 +0530

    Add files via upload
    changes

---
+++ Taskfinalsamy/src/main/java/com/example/bio/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.bio;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+    //Initalizing
+    RecyclerView recycle;
+    ArrayList<Mainmodel> mainmodel;
+    Mainadapter mainadapter;
+    View dialog;
+    Activity activity;
+
+
+    //create integer array
+    Integer [] r = {R.drawable.burger,R.drawable.pizza, R.drawable.burger,R.drawable.snacks,R.drawable.burger,R.drawable.pizza,R.drawable.shusi,R.drawable.snacks,R.drawable.burger,R.drawable.pizza, R.drawable.burger,R.drawable.snacks};
+    String [] langname = {"burger","pizza","shusi","snacks","burger","pizza","shusi","snacks","burger","pizza","shusi","snacks"};
+
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        activity= MainActivity.this;
+        recycle = findViewById( R.id.recyclerview);
+//        dialog = findViewById( R.id.textView);
+
+
+
+
+        //array list
+        mainmodel = new ArrayList<>();
+
+
+        //for condition
+        for (int i =0;i<langname.length;i++){
+
+            Mainmodel model = new Mainmodel(r[i],langname[i] );
+            mainmodel.add(model);
+
+
+
+        }
+
+        //Horizontal layout
+        LinearLayoutManager layoutManager = new LinearLayoutManager( MainActivity.this,LinearLayoutManager.HORIZONTAL,false );
+        recycle.setLayoutManager( layoutManager ); // it set into recycler view
+
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView( recycle );
+
+        mainadapter = new Mainadapter( mainmodel, MainActivity.this,activity );
+
+        recycle.scrollToPosition( 0 );
+
+
+
+        //set adaptor to recycleview
+        recycle.setAdapter( mainadapter );
+    }
+
+
+
+}
---
+++ Taskfinalsamy/src/main/java/com/example/bio/Mainadapter.java
@@ -0,0 +1,131 @@
+package com.example.bio;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+public class Mainadapter extends RecyclerView.Adapter<Mainadapter.ViewHolder> {
+    ArrayList<Mainmodel> mainmodels;
+    Context context;
+    Activity activity;
+
+    public Mainadapter(ArrayList<Mainmodel> mainmodels, Context context, Activity activity) {
+        this.mainmodels = mainmodels;
+        this.context = context;
+        this.activity = activity;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from( parent.getContext() ).inflate( R.layout.row_item, parent, false );
+        return new ViewHolder( view );
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.imageView.setImageResource( mainmodels.get( position ).getR() );
+        holder.textView.setText( mainmodels.get( position ).getLangname() );
+
+        holder.itemView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                holder.itemView.scrollBy(position(0),0);
+                holder.itemView.scrollTo(100,0);
+
+
+
+            }
+
+        } );
+
+
+        holder.itemView.setOnLongClickListener( new View.OnLongClickListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public boolean onLongClick(View view) {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+                LayoutInflater inflater = activity.getLayoutInflater();
+
+                View dialogView = inflater.inflate(R.layout.sample_dialogue, null);
+                dialogBuilder.setView(dialogView);
+
+                TextView textView = dialogView.findViewById(R.id.textView);
+                textView.setText("Hey you have selected the item "+ mainmodels.get( position ).getLangname());
+
+                ImageView imageView = dialogView.findViewById(R.id.images_icon);
+                imageView.setImageResource(mainmodels.get( position ).getR());
+
+                Button button = dialogView.findViewById(R.id.button_ok);
+                button.setText( "Selected item is "+ mainmodels.get( position ).getLangname());
+
+                AlertDialog alertDialog = dialogBuilder.create();
+                button.setOnClickListener( new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        alertDialog.dismiss();
+
+                    }
+                } );
+                alertDialog.show();
+                return false;
+
+//                AlertDialog.Builder builder = new AlertDialog.Builder( context );
+//                builder.setMessage( "Hey you have selected the item "+ mainmodels.get( position ).getLangname().toUpperCase() );
+//                builder.setCancelable( true );
+//
+//
+//                builder.setPositiveButton( "yes i have selected "+mainmodels.get( position ).getLangname().toUpperCase(), new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        return;
+//
+//                    }
+//                } );
+//
+//                AlertDialog alertDialog = builder.create();
+//                alertDialog.show();
+//                return false;
+            }
+        } );
+    }
+
+
+
+
+
+    @Override
+    public int getItemCount() {
+
+        return mainmodels.size();
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public Object setOnclick;
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super( itemView );
+            //getting image and text view
+            imageView = itemView.findViewById( R.id.image_view );
+            textView = itemView.findViewById( R.id.Textview );
+        }
+    }
+}
